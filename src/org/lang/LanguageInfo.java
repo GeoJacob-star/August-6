9a8e92c37deead1d0a915ca3070759564b662afb
username <emailid>
2024-08-06 18:52:22 +0530

    push1

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class LanguageInfo extends StateDetails {
+  private void tamilLanguage() {
+	System.out.println("Tamil language students : 2500");
+}
+  private void englishLanguage() {
+	System.out.println("English language students : 1000");
+}
+  private void hindiLanguage() {
+	System.out.println("Hindi language students : 100");
+}
+  public static void main(String[] args) {
+	  LanguageInfo l = new LanguageInfo();
+	  l.tamilLanguage();
+	  l.englishLanguage();
+	  l.hindiLanguage();
+	  l.southIndia();
+	  l.northIndia();
+}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,10 @@
+package org.lang;
+
+public class StateDetails {
+	public void southIndia() {
+	System.out.println("South India has Tamilnadu");
+}
+	public void northIndia() {
+	System.out.println("North India has Bihar");
+}
+}
